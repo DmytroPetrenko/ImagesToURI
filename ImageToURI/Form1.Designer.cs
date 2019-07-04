e027525a423ada73ad039b646fb2d52c4c755fb9
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2019-07-04 11:19:44 +0300

    Add project files.

---
+++ ImageToURI/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ImageToURI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.btnSelectImageFolder = new System.Windows.Forms.Button();
+            this.btn_Select_Root_Folder = new System.Windows.Forms.Button();
+            this.btnJustDoIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Преобразовать в URI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(58, 358);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(191, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Выбрать файл html";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Location = new System.Drawing.Point(58, 12);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(684, 308);
+            this.txtEditor.TabIndex = 2;
+            // 
+            // btnSelectImageFolder
+            // 
+            this.btnSelectImageFolder.Location = new System.Drawing.Point(255, 358);
+            this.btnSelectImageFolder.Name = "btnSelectImageFolder";
+            this.btnSelectImageFolder.Size = new System.Drawing.Size(191, 23);
+            this.btnSelectImageFolder.TabIndex = 3;
+            this.btnSelectImageFolder.Text = "Выбрать папку с картинками";
+            this.btnSelectImageFolder.UseVisualStyleBackColor = true;
+            this.btnSelectImageFolder.Click += new System.EventHandler(this.BtnSelectImageFolder_Click);
+            // 
+            // btn_Select_Root_Folder
+            // 
+            this.btn_Select_Root_Folder.Location = new System.Drawing.Point(520, 358);
+            this.btn_Select_Root_Folder.Name = "btn_Select_Root_Folder";
+            this.btn_Select_Root_Folder.Size = new System.Drawing.Size(222, 23);
+            this.btn_Select_Root_Folder.TabIndex = 4;
+            this.btn_Select_Root_Folder.Text = "Выбрать папку для преобразования";
+            this.btn_Select_Root_Folder.UseVisualStyleBackColor = true;
+            this.btn_Select_Root_Folder.Click += new System.EventHandler(this.Btn_Select_Root_Folder_Click);
+            // 
+            // btnJustDoIt
+            // 
+            this.btnJustDoIt.Location = new System.Drawing.Point(520, 387);
+            this.btnJustDoIt.Name = "btnJustDoIt";
+            this.btnJustDoIt.Size = new System.Drawing.Size(222, 23);
+            this.btnJustDoIt.TabIndex = 5;
+            this.btnJustDoIt.Text = "Преобразовать всё";
+            this.btnJustDoIt.UseVisualStyleBackColor = true;
+            this.btnJustDoIt.Click += new System.EventHandler(this.BtnJustDoIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnJustDoIt);
+            this.Controls.Add(this.btn_Select_Root_Folder);
+            this.Controls.Add(this.btnSelectImageFolder);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.Button btnSelectImageFolder;
+        private System.Windows.Forms.Button btn_Select_Root_Folder;
+        private System.Windows.Forms.Button btnJustDoIt;
+    }
+}
+
---
+++ ImageToURI/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToURI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // файл для обработки и папка с изображениями
+        string filePath, folderPath;
+
+        string rootFolderPath;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            TransformToURL();
+            Process.Start(filePath);
+        }
+
+        private string[] DeleteMetaInFile(string fileFullName)
+        {
+            string[] text = File.ReadAllLines(fileFullName);
+
+            string pattern = @"<meta[\s\S]";
+
+            Regex regex = new Regex(pattern);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (regex.IsMatch(text[i]))
+                {
+                    text[i] = Regex.Replace(text[i], pattern, "");
+                }
+            }            
+
+            return text;
+        }
+
+        private void TransformToURL()
+        {
+            string[] text = File.ReadAllLines(filePath);
+            string[] text1 = text;
+
+            DirectoryInfo directory = new DirectoryInfo(folderPath);
+            var filesInDirectory = directory.GetFiles();
+            string[] filesNames = new string[filesInDirectory.Length];
+            string[] filesFullNames = new string[filesInDirectory.Length];
+            string[] patterns = new string[filesInDirectory.Length];
+
+            int filesCounter = 0;
+            foreach (var file in filesInDirectory)
+            {
+                filesNames[filesCounter] = file.Name;
+                filesFullNames[filesCounter] = file.FullName;
+                filesCounter++;
+            }
+            filesCounter = 0;
+
+            // Создание необходимого количества патернов
+            for (int i = 0; i < filesNames.Length; i++)
+            {
+                patterns[filesCounter] = @"<img(\s+)width(.{1,})" + filesNames[filesCounter] + "\">";
+                filesCounter++;
+            }
+
+            int patternCounter;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Regex regex;
+
+                // Совмещение строк с названием картинки
+                foreach (var fileName in filesNames)
+                {
+                    regex = new Regex(fileName);
+                    if (regex.IsMatch(text[i]))
+                    {
+                        text1[i] = text1[i - 1] + " " + text[i];
+                        text1[i - 1] = "";
+                    }
+                }
+
+                // Замещение строк с картинками на URI
+                patternCounter = 0;
+                foreach (var pattern in patterns)
+                {
+                    regex = new Regex(pattern);
+                    if (regex.IsMatch(text[i]))
+                    {
+                        text1[i] = Regex.Replace(text[i], pattern, ImgToURI(filesFullNames[patternCounter]));
+                    }
+                    patternCounter++;
+                }
+
+
+
+            }
+
+
+            File.WriteAllLines(filePath, text1);
+        }
+
+        private void BtnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            txtEditor.Text += "Выбран " + openFileDialog.FileName;
+            filePath = openFileDialog.FileName;
+        }
+
+        private static string ImgToURI(string imgFile)
+        {
+            return "<img src=\"data:image/"
+                        + Path.GetExtension(imgFile).Replace(".", "")
+                        + ";base64,"
+                        + Convert.ToBase64String(File.ReadAllBytes(imgFile)) + "\" />";
+        }
+          
+        private void BtnSelectImageFolder_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                dialog.ShowDialog();
+                txtEditor.Text += "Выбрана папка: " + dialog.SelectedPath;
+                folderPath = dialog.SelectedPath;
+            }
+        }
+
+        private void BtnJustDoIt_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo rootDirectory = new DirectoryInfo(rootFolderPath);
+            var directories = rootDirectory.GetDirectories();
+            bool checkExtension;
+            foreach (var directory in directories)
+            {
+                checkExtension = false;
+
+                if (directory.GetDirectories().Length != 0)
+                {
+                    // Получение папки с изображениями
+                    var subdirectories = directory.GetDirectories();
+
+                    
+
+                    foreach (var subdirectory in subdirectories)
+                    {
+                        foreach (var file in subdirectory.GetFiles())
+                        {
+                            if (file.Extension == ".png" || file.Extension == ".jpg" || file.Extension == ".jpeg")
+                            {
+                                checkExtension = true;
+                                break;
+                            }
+                        }
+                        folderPath = subdirectory.FullName;
+                    }
+
+                }
+
+                // Получение html файла для обработки и удаление meta в файлах
+                var files = directory.GetFiles();
+                foreach (var file in files)
+                {
+                    filePath = file.FullName;
+                    DeleteMetaInFile(filePath);
+                }
+                                       
+                if (directory.GetDirectories().Length != 0)
+                {
+                    TransformToURL();
+
+                    if (checkExtension == true)
+                    {
+                        Process.Start(filePath);
+                    }
+                }
+                }
+            }
+        }
+
+        private void Btn_Select_Root_Folder_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                dialog.ShowDialog();
+                txtEditor.Text += "Выбрана корневая папка: " + dialog.SelectedPath;
+                rootFolderPath = dialog.SelectedPath;
+            }
+        }
+    }
+
+}
